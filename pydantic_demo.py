78132b5b42063221186ff7823e1dbdaef92f2712
chen4c <chen4393c@outlook.com>
2026-06-08 16:37:06 +0800

    refactor: remove redundant User class definition and clean up print statements

--- pydantic_demo.py
+++ pydantic_demo.py
@@ -17,21 +17,13 @@ external_data = {
 }
 
 user = User(**external_data)
-print(user.id)
 print(user.model_dump())
+print()
 
 # continuing the above example...
 
 from datetime import datetime
 from pydantic import BaseModel, PositiveInt, ValidationError
-
-
-class User(BaseModel):
-    id: int
-    name: str = "John Doe"
-    signup_ts: datetime | None
-    tastes: dict[str, PositiveInt]
-
 
 external_data = {"id": "not an int", "tastes": {}}
 
